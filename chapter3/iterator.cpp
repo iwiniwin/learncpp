5f6469316aa3580fac07a4c5e55bcd85f64d5365
iwiniwin <iwiniwin@foxmail.com>
2020-11-27 20:42:28 +0800

    补充迭代器用例

--- chapter3/iterator.cpp
+++ chapter3/iterator.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <iterator>
 #include <deque>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
 void print(vector<int> &vec){
@@ -103,12 +105,90 @@ void test2(){
     insert_iter ++;
     *insert_iter = -2;
     print(veci2);  // 0 -1 -2 1 2 3 4 5
+    insert_iter = -3;  // 可以忽略解引用操作
+    print(veci2);  // 0 -1 -2 -3 1 2 3 4 5
+}
+
+class Test {
+public:
+    int a = 0;
+};
+
+// 流迭代器
+void test3(){
+    // ostream_iterator<int> out_iter;  // error : 不允许空的或表示尾后的ostream_iterator，必须绑定到流
+    ostream_iterator<int> out_iter(cout);
+    ostream_iterator<int> out_iter2(cout, " ");  // 在输出每个元素后都会添加此字符串
+
+    // string s = "ss";
+    // ostream_iterator<int> out_iter3(cout, s);  // error : s必须是c风格字符串
+
+    vector<int> vec2 = {1, 2, 3};
+    for(auto e : vec2){
+        *out_iter++ = e;  // 输出e，123
+        // *out_iter = e;  // 等价于上面的写法
+        // out_iter = e;  // 等价于上面的写法，解引用和递增原酸，可以忽略，实际上它们什么也不做，但仍建议写成第一种样式，更清晰
+    }
+    cout << endl;
+
+    copy(vec2.begin(), vec2.end(), out_iter);  // 利用copy算法，输出vec中元素
+    cout << endl;
+
+    // todo，利用流迭代器处理类类型
+    // ostream_iterator<Test> out_iter4(cout);
+    // Test t();
+    // out_iter4 = t;
+
+
+    istream_iterator<int> int_iter(cin);  // 将迭代器绑定到cin，从cin读取int，istream_iterator使用>>来读取流
+    istream_iterator<int> int_eof;   // 默认初始化迭代器，相当于尾后迭代器
+    while(int_iter != int_eof){
+        cout << *int_iter++ << endl;  // 获得从流读取的值，且迭代器递增
+    }
+
+    istream_iterator<int> in_iter(cin), eof;
+    vector<int> vec(in_iter, eof);  // 利用流迭代器构造vector
+
+    // 使用算法操作流迭代器
+    istream_iterator<int> in(cin), ieof;
+    cout << accumulate(in, ieof, 0) << endl;
+}
+
+// 反向迭代器
+void test4(){
+    vector<int> vec = {1, 2, 3, 4};
+    for(auto r_iter = vec.rbegin(); r_iter != vec.rend(); ++r_iter){  // 对于反向迭代器而言，递增和递减的含义颠倒了过来
+        cout << *r_iter << " ";  // 4 3 2 1
+    }
+    cout << endl;
+
+    sort(vec.begin(), vec.end());
+    print(vec);  // 1 2 3 4
+
+    sort(vec.rbegin(), vec.rend());
+    print(vec);  // 4 3 2 1
+
+
+    string line = "abc,def,ghi,jkl";
+    auto comma = find(line.begin(), line.end(), ',');
+    cout << string(line.begin(), comma) << endl;  // abc，打印逗号前的第一个单词
+
+    auto comma2 = find(line.rbegin(), line.rend(), ',');
+    cout << string(line.rbegin(), comma2) << endl;  // lkj，打印逗号后的最后一个单词，注意，输出的是逆序，而不是jkl
+    // 注意comma2也同样是一个反向迭代器
+    auto comma3 = comma2.base(); // 将返回一个正常的迭代器
+    cout << string(comma3, line.end()) << endl;  // jkl
+
+    // 普通迭代器和反向迭代器的关系反映了左闭合区间的特性
+    cout << *comma2 << " " << *comma3 << endl;  // , j  注意，comma2和comma3指向的是不同的元素，这种不同保证了元素范围无论是正向处理还是反向处理都是相同的
 }
 
 int main()
 {
     // test1();
-    test2();
+    // test2();
+    // test3();
+    test4();
 
 
     system("pause");
